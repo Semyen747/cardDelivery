a5b57d18e9ca0c3e9a5f1dfc8181a816e3b0a20f
Semyen747 <combatpilot45@gmail.com>
2023-03-22 01:58:14 +0300

    fix for planning days, cleanup method

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SetValueOptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -12,7 +13,7 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class cardDeliveryTest {
+public class CardDeliveryTest {
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
@@ -25,14 +26,14 @@ public class cardDeliveryTest {
     @Test
     void shouldRequest() {
         LocalDate date = LocalDate.now();
-        date = date.plusDays(3);
+        date = date.plusDays(5);
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Санкт-Петербург");
-        $("[data-test-id=date] input").setValue(date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
         $("[data-test-id=name] input").setValue("Петров Олег");
         $("[data-test-id=phone] input").setValue("+79965898790");
         $("[data-test-id=agreement]").click();
-        String planningDate = generateDate(3);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        String planningDate = generateDate(5);
         $("[data-test-id=date] input").setValue(planningDate);
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(14));
